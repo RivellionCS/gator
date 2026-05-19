e79f011a94e6175d12da077142015288a45888b3
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-05-19 00:07:40 -0300

    create write function and configFileName const

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,6 +6,8 @@ import (
 	"os"
 )
 
+const configFileName = ".gatorconfig.json"
+
 type Config struct {
 	DbURL string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
@@ -34,11 +36,25 @@ func Read() Config {
 
 func getConfigFilePath() (string, error) {
 	config_file_path, err := os.UserHomeDir()
-	config_file_path += "/.gatorconfig"
+	config_file_path += "/" + configFileName
 	return config_file_path, err
 }
 
 func (c Config) SetUser(username string) {
 	c.CurrentUserName = username
 	write(c)
+}
+
+func write(cfg Config) error {
+	jsonData, err := json.Marshal(cfg)
+	if err != nil {
+		return fmt.Errorf("Error could not convert to json: %v\n", err)
+	}
+	config_file_path, _ := os.UserHomeDir()
+	config_file_path += "/" + configFileName
+	err = os.WriteFile(config_file_path, jsonData, 0664)
+	if err != nil {
+		return fmt.Errorf("Error writing to the config file: %v\n", err)
+	}
+	return err
 }
