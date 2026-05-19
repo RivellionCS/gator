2471a8f0602925984a1530e0600d78419be73096
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-05-19 00:20:57 -0300

    change main to test read and write

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/RivellionCS/gator/internal/config"
+)
+
 func main() {
-	
+	new_config := config.Read()
+	new_config.SetUser("RivellionCS")
+	new_config = config.Read()
+	fmt.Printf("%v\n", new_config)
 }
