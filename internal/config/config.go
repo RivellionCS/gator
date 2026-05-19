e78f66bbe6121bb32cba9478f684fe1a8628c93b
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-05-18 23:38:46 -0300

    create Read function

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,33 @@
 package config
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 type Config struct {
 	DbURL string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
+}
+
+func Read() Config {
+	config_path, err := getConfigFilePath()
+	if err != nil {
+		fmt.Printf("Error getting file path: %v\n", err)
+	}
+
+	data, err := os.ReadFile(config_path)
+	if err != nil {
+		fmt.Printf("Error reading file path: %v\n", err)
+	}
+
+	new_config := Config{}
+
+	err = json.Unmarshal(data, &new_config)
+	if err != nil {
+		fmt.Printf("Error parsing JSON: %v\n", err)
+	}
+
+	return new_config
 }
