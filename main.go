48c9596773a08da675f4341d05d4c15fd856bf20
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-05-20 23:27:42 -0300

    change main to use arguments to run

--- main.go
+++ main.go
@@ -2,13 +2,38 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/RivellionCS/gator/internal/config"
 )
 
 func main() {
-	new_config := config.Read()
-	new_config.SetUser("RivellionCS")
-	new_config = config.Read()
-	fmt.Printf("%v\n", new_config)
+	newConfig, err := config.Read()
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+	newState := state{
+		config: &newConfig,
+	}
+	newCommands := commands{
+		command_list: map[string]func(*state, command) error{},
+	}
+	newCommands.register("login", handlerLogin)
+	arguments := os.Args
+	if len(arguments) < 2 {
+		fmt.Println("arguments can't be less than 2")
+		os.Exit(1)
+	}
+	commandName := arguments[1]
+	commandSlice := arguments[2:]
+	newCommand := command{
+		name: commandName,
+		arguments: commandSlice,
+	}
+	err = newCommands.run(&newState, newCommand)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
 }
