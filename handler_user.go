7ebe6f4332555e5a71301e9a8946b535ce60c294
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-05-22 17:10:24 -0300

    add handleRegister function

--- handler_user.go
+++ handler_user.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"time"
+
+	"github.com/RivellionCS/gator/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -11,5 +16,28 @@ func handlerLogin(s *state, cmd command) error {
 	}
 	s.cfg.SetUser(cmd.arguments[0])
 	fmt.Println("the user has been set")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.arguments) == 0 {
+		return errors.New("the register handler expects a single argument, the name")
+	}
+	currentTime := time.Now()
+	params := database.CreateUserParams{
+		ID: uuid.New(),
+		CreatedAt: currentTime,
+		UpdatedAt: currentTime,
+		Name: cmd.arguments[0],
+	}
+	user, err := s.db.CreateUser(context.Background(), params)
+	if err != nil {
+		return fmt.Errorf("couldn't create user: %w", err)
+	}
+	err = s.cfg.SetUser(cmd.arguments[0])
+	if err != nil {
+		return fmt.Errorf("couldn't set user %w", err)
+	}
+	fmt.Printf("A new user has been created:\n%v\n", user)
 	return nil
 }
