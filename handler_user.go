6fc87cf6f644f9c662694d1a8c42c27d5b577909
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-05-23 16:56:42 -0300

    add command for listing all users

--- handler_user.go
+++ handler_user.go
@@ -57,3 +57,18 @@ func handlerReset(s *state, cmd command) error {
 	fmt.Println("sucessfully deleted all users from table")
 	return nil
 }
+
+func handlerListUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't get all the users: %w", err)
+	}
+	for _, user := range users {
+		if user.Name == s.cfg.CurrentUserName {
+			fmt.Printf("* %v (current)\n", user.Name)
+		} else {
+			fmt.Printf("* %v\n", user.Name)
+		}
+	}
+	return nil
+}
