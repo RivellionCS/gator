9d7a5f88d520a7b9b9a2218d84e4fcf900c05641
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-05-23 16:28:34 -0300

    create handlerReset function

--- handler_user.go
+++ handler_user.go
@@ -48,3 +48,12 @@ func handlerRegister(s *state, cmd command) error {
 	fmt.Printf("A new user has been created:\n%v\n", user)
 	return nil
 }
+
+func handlerReset(s *state, cmd command) error {
+	err := s.db.DeleteUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't delete all users: %w", err)
+	}
+	fmt.Println("sucessfully deleted all users from table")
+	return nil
+}
