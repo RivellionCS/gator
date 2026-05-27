944ed71694674b19936c80913583dd5bccf05df1
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-05-26 22:21:27 -0300

    create handlerAgg function

--- handler_user.go
+++ handler_user.go
@@ -72,3 +72,12 @@ func handlerListUsers(s *state, cmd command) error {
 	}
 	return nil
 }
+
+func handlerAgg(s *state, cmd command) error {
+	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	if err != nil {
+		return fmt.Errorf("error fetching feed: %v\n", err)
+	}
+	fmt.Printf("Printing Feed:\n%v\n", feed)
+	return nil
+}
