706bf9cc7576d82579ac89b873392d3395fe2861
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-05-26 22:13:39 -0300

    set header and unescape string for description and title

--- rss_feed.go
+++ rss_feed.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/xml"
 	"fmt"
+	"html"
 	"io"
 	"net/http"
 	"time"
@@ -28,26 +29,34 @@ type RSSFeed struct {
 func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
 	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
 	if err != nil {
-		return nil, fmt.Errorf("Request error: %v\n", err)
+		return nil, fmt.Errorf("Request error: %v", err)
 	}
+	req.Header.Set("User-Agent", "gator")
 
 	client := &http.Client{Timeout: 10 * time.Second}
 	resp, err := client.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("Response error: %v\n", err)
+		return nil, fmt.Errorf("Response error: %v", err)
 	}
 	defer resp.Body.Close()
 
 	data, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("Data reading error: %v\n", err)
+		return nil, fmt.Errorf("Data reading error: %v", err)
 	}
 
 	feed := &RSSFeed{}
 	err = xml.Unmarshal(data, feed)
 	if err != nil {
-		return nil, fmt.Errorf("Unmarshaling error: %v\n", err)
+		return nil, fmt.Errorf("Unmarshaling error: %v", err)
 	}
-	
+
+	feed.Channel.Title = html.UnescapeString(feed.Channel.Title)
+	feed.Channel.Description = html.UnescapeString(feed.Channel.Description)
+	for i := range feed.Channel.Item {
+		feed.Channel.Item[i].Title = html.UnescapeString(feed.Channel.Item[i].Title)
+		feed.Channel.Item[i].Description = html.UnescapeString(feed.Channel.Item[i].Description)
+	}
+
 	return feed, nil
 }
