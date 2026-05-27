05feeeff76f5e80a188c69d42f37ecc73c5521ed
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-05-26 21:30:31 -0300

    add fetchFeed function

--- rss_feed.go
+++ rss_feed.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"context"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
 type RSSItem struct {
 	Title       string `xml:"title"`
 	Link        string `xml:"link"`
@@ -14,4 +23,31 @@ type RSSFeed struct {
 		Description string    `xml:"description"`
 		Item        []RSSItem `xml:"item"`
 	} `xml:"channel"`
+}
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return nil, fmt.Errorf("Request error: %v\n", err)
+	}
+
+	client := &http.Client{Timeout: 10 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("Response error: %v\n", err)
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Data reading error: %v\n", err)
+	}
+
+	feed := &RSSFeed{}
+	err = xml.Unmarshal(data, feed)
+	if err != nil {
+		return nil, fmt.Errorf("Unmarshaling error: %v\n", err)
+	}
+	
+	return feed, nil
 }
