caedff1bd14d3bc96139e50a31db780543fb697a
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-05-28 04:13:30 -0300

    create query for all feeds for a user

--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -17,3 +17,10 @@ SELECT
 FROM inserted_feed_follow
 JOIN feeds ON inserted_feed_follow.feed_id = feeds.id
 JOIN users ON inserted_feed_follow.user_id = users.id;
+
+-- name: GetFeedFollowsForUser :many
+SELECT feed_follows.*, feeds.name AS feed_name, users.name AS user_name
+FROM feed_follows 
+JOIN feeds ON feed_follows.feed_id = feeds.id
+JOIN users ON feed_follows.user_id = users.id
+WHERE user_id = $1;
