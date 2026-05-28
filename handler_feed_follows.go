54ef8718b7a594595a88d731bc37726b7c9801fe
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-05-28 04:00:00 -0300

    create follow handler

---
+++ handler_feed_follows.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/RivellionCS/gator/internal/database"
+	"github.com/google/uuid"
+)
+
+func handlerFollow(s *state, cmd command) error {
+	if len(cmd.arguments) != 1 {
+		return fmt.Errorf("The follow command requires a url argument")
+	}
+
+	url := cmd.arguments[0]
+	feed, err := s.db.GetFeedByUrl(context.Background(), url)
+	if err != nil {
+		return fmt.Errorf("error getting feed by url: %v", err)
+	}
+
+	username := s.cfg.CurrentUserName
+	user, err := s.db.GetUser(context.Background(), username)
+	if err != nil {
+		return fmt.Errorf("error getting user by name: %v", err)
+	}
+
+	currentTime := time.Now()
+	params := database.CreateFeedFollowParams{
+		ID: uuid.New(),
+		CreatedAt: currentTime,
+		UpdatedAt: currentTime,
+		UserID: user.ID,
+		FeedID: feed.ID,
+	}
+	feedFollow, err := s.db.CreateFeedFollow(context.Background(), params)
+	if err != nil {
+		return fmt.Errorf("error creating feed follow: %v", err)
+	}
+	fmt.Printf("Feed Name: %v\n", feedFollow.FeedName)
+	fmt.Printf("Current User: %v\n", feedFollow.UserName)
+	return nil
+}
