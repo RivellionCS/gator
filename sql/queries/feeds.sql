8225faf5d8dcfb05fac9b41df996fd928ea88e3c
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-05-28 03:10:14 -0300

    add query to get feed by url

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -14,3 +14,8 @@ RETURNING *;
 SELECT feeds.name AS feeds_name, feeds.url AS feeds_url, users.name AS user_name
 FROM feeds
 JOIN users ON feeds.user_id = users.id;
+
+-- name: GetFeedByUrl :one
+SELECT * FROM feeds
+WHERE url = $1
+LIMIT 1;
