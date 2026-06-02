623413e7480047e0bb6a34516a3fae57a603ed46
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-06-02 04:25:53 -0300

    create query for getting posts for a user

--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -11,3 +11,11 @@ VALUES (
     $8
 )
 RETURNING *;
+
+-- name: GetPostsForUser :many
+SELECT posts.*
+FROM posts
+JOIN feed_follows ON posts.feed_id = feed_follows.feed_id
+WHERE user_id = $1
+ORDER BY updated_at DESC
+LIMIT $2;
