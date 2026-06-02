036014d1ce2c6c743b7c924fa118a7c962bda479
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-06-02 03:36:14 -0300

    create scrape function

--- handler_agg.go
+++ handler_agg.go
@@ -7,10 +7,32 @@ import (
 
 
 func handlerAgg(s *state, cmd command) error {
+	if len(cmd.arguments) != 1 {
+		return fmt.Errorf("the agg command expects 1 time argument")
+	}
 	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
 	if err != nil {
 		return fmt.Errorf("error fetching feed: %v\n", err)
 	}
 	fmt.Printf("Printing Feed:\n%v\n", feed)
+	return nil
+}
+
+func scrapeFeeds(s *state) error {
+	feed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return fmt.Errorf("error getting next feed: %v", err)
+	}
+	err = s.db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil {
+		return fmt.Errorf("error marking feed as fetched: %v", err)
+	}
+	rssFeed, err := fetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		return fmt.Errorf("error fetching feed by url: %v", err)
+	}
+	for _, item := range rssFeed.Channel.Item {
+		fmt.Println(item.Title)
+	}
 	return nil
 }
